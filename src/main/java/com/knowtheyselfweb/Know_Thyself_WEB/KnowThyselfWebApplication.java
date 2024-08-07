c6bc8246f7b91eda14d95913bd8695c8fe6d0d49
sharktrexer <sharktrexer@gmail.com>
2024-08-06 18:58:55 -0700

    spring boot web app template

---
+++ src/main/java/com/knowtheyselfweb/Know_Thyself_WEB/KnowThyselfWebApplication.java
@@ -0,0 +1,13 @@
+package com.knowtheyselfweb.Know_Thyself_WEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowThyselfWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowThyselfWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knowtheyselfweb/Know_Thyself_WEB/KnowThyselfWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowtheyselfweb.Know_Thyself_WEB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowThyselfWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
